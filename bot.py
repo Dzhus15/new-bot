e68ccd0be892a6db54aa1d4e7b3bbcbedeb1681f
Dzhus15 <133809675+Dzhus15@users.noreply.github.com>
2024-06-18 17:42:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import os
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from dotenv import load_dotenv
 
@@ -19,7 +19,7 @@ if not TOKEN:
 # Обработчик команды /start
 def start(update: Update, context: CallbackContext):
     keyboard = [
-        [InlineKeyboardButton("Открыть веб-интерфейс", web_app=telegram.WebAppInfo(url="https://dzhus15.github.io/Telegram-bot/index.html"))],
+        [InlineKeyboardButton("Открыть веб-интерфейс", web_app=WebAppInfo(url="https://dzhus15.github.io/Telegram-bot/index.html"))],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     update.message.reply_text(
